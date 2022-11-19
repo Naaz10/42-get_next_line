1087fd1131201323e9f8e063f905e4df0de1bbb7
Naaz10 <109809806+Naaz10@users.noreply.github.com>
2022-11-19 17:59:39 +0400

    Add files via upload
    Norminetted and tester passed.

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nafrin <nafrin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 16:18:02 by nafrin            #+#    #+#             */
+/*   Updated: 2022/11/19 16:34:19 by nafrin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 // join and free
@@ -18,20 +30,16 @@ char	*ft_next(char *buffer)
 	char	*line;
 
 	i = 0;
-	// find len of first line
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	// if eol == \0 return NULL
 	if (!buffer[i])
 	{
 		free(buffer);
 		return (NULL);
 	}
-	// len of file - len of firstline + 1
 	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
 	i++;
 	j = 0;
-	// line == buffer
 	while (buffer[i])
 		line[j++] = buffer[i++];
 	free(buffer);
@@ -39,28 +47,31 @@ char	*ft_next(char *buffer)
 }
 
 // take line for return
+// if no line return NULL ->>  if (!buffer[i])
+// go to the eol          ->> while (buffer[i] && buffer[i] != '\n')
+// malloc to eol          ->>  line = ft_calloc(i + 2, sizeof(char));
+// if eol is \0 or \n, replace eol by \n  
+// -> if (buffer[i] && buffer[i] == '\n')
+// malloc if res dont exist    ->> if (!res) res = ft_calloc(1, 1);
+// malloc buffer               ->> buffer = ft_calloc
+
 char	*ft_line(char *buffer)
 {
 	char	*line;
 	int		i;
 
 	i = 0;
-	// if no line return NULL
 	if (!buffer[i])
 		return (NULL);
-	// go to the eol
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	// malloc to eol
 	line = ft_calloc(i + 2, sizeof(char));
 	i = 0;
-	// line = buffer
 	while (buffer[i] && buffer[i] != '\n')
 	{
 		line[i] = buffer[i];
 		i++;
 	}
-	// if eol is \0 or \n, replace eol by \n
 	if (buffer[i] && buffer[i] == '\n')
 		line[i++] = '\n';
 	return (line);
@@ -71,26 +82,20 @@ char	*read_file(int fd, char *res)
 	char	*buffer;
 	int		byte_read;
 
-	// malloc if res dont exist
 	if (!res)
 		res = ft_calloc(1, 1);
-	// malloc buffer
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	byte_read = 1;
 	while (byte_read > 0)
 	{
-		// while not eof read
 		byte_read = read(fd, buffer, BUFFER_SIZE);
 		if (byte_read == -1)
 		{
 			free(buffer);
 			return (NULL);
 		}
-		// 0 to end for leak
 		buffer[byte_read] = 0;
-		// join and free
 		res = ft_free(res, buffer);
-		// quit if \n find
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
@@ -103,7 +108,6 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	// error handling
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	buffer = read_file(fd, buffer);
