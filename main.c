c5a0bda9102ffe5ead4651e1d89f840b04c30337
Naaz10 <109809806+Naaz10@users.noreply.github.com>
2023-01-07 21:10:55 +0400

    Main file added

--- main.c
+++ main.c
@@ -4,17 +4,24 @@
 
 int main(void)
 {
-    int fd;
+    int fd1;
+    int fd2;
     char *line;
 
-    fd = open("test.txt",O_RDONLY);
-    while ((line = get_next_line(fd))!= NULL)
+    fd1 = open("test.txt",O_RDONLY);
+    fd2 = open("test2.txt",O_RDONLY);
+    // while ((line = get_next_line(fd))!= NULL)
+    for (int i = 0; i < 3; i++)
     {
+        line = get_next_line(fd1);
+        printf("%s",line);
+        free(line);
+        line = get_next_line(fd2);
         printf("%s",line);
         free(line);
     }
-    printf("%s",line);
-    free(line);
-    close(fd);
+    // free(line);
+    close(fd1);
+    close(fd2);
     return(0);
 }
